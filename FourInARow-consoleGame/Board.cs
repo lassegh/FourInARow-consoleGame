e8b4ba4798f956f19ca8734475bfa8436a693c85
Michael Kjergaard <KjergaardVFX@gmail.com>
2018-10-12 10:46:13 +0200

    Made the board class

---
+++ FourInARow-consoleGame/Board.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace FourInARow_consoleGame
+{
+    public class Board
+    {
+        public Board()
+        {
+            char[,] twoDimensionalArray = new char[6, 6];
+
+            /// <summary>
+            /// get a singel value from our two dimensional array
+            /// </summary>
+            /// <param name="x">x axis</param>
+            /// <param name="y">y axis</param>
+            /// <returns></returns>
+            public char getSingleValue(int x, int y)
+            {
+                return twoDimensionalArray[x, y];
+            }
+
+            /// <summary>
+            /// Set a single value our two dimensional array
+            /// </summary>
+            /// <param name="x">x axis position</param>
+            /// <param name="y">y axis position</param>
+            /// <param name="value">the char you want to set</param>
+            /// <returns></returns>
+            public char setSingleValue(int x, int y, char value)
+            {
+                twoDimensionalArray[x, y] = value;
+            }
+
+        }
+
+        public void AddPiece(var formOfPiece, int kolonne)
+        {
+
+        }
+
+        private bool BoardFull()
+        {
+
+        }
+
+        public bool KolonneFull()
+        {
+
+        }
+    }
+    }
+}
