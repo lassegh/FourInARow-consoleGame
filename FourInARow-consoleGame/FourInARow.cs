858b071d0aaa241f480c1006bb8dbfb2adca13e2
Kjergaard <KjergaardVFX@gmail.com>
2018-10-17 12:22:40 +0200

    up to date FourInARow
    Så mit github ikke stikker helt af! :)

--- FourInARow-consoleGame/FourInARow.cs
+++ FourInARow-consoleGame/FourInARow.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,6 @@ namespace FourInARow_consoleGame
     {
         List<Player> players = new List<Player>();
         public Board board;
-        //public char emptySpace = 'O'; Hvorfor er den blevet dubleret?
         public Player currentPlayer;
        
 
@@ -21,27 +21,33 @@ namespace FourInARow_consoleGame
         {
             board = new Board(this);
 
-            currentPlayer = players.First(); // Bør currentPlayer ikke først initialiseres med en af spillerne EFTER de er oprettet?
+            
             players.Add(new Player("Lars", 'X'));
             players.Add(new Player("Martin", 'B'));
+            currentPlayer = players.First();
 
 
-
-            while (!GameOver.CheckFourInARow(board.boardArray)) // Hvorfor køres loopet i constructoren?
+            while (!GameOver.CheckFourInARow(board.boardArray))
             {
-                // spørg currentplaye
-                if (!board.KolonneFull(kolonne)) // Her er der vist ingen attribut?
+                
+                if (!board.BoardFull()) // nu tjekker vi i stedet om boardet er fuldt -lars
                 {
-                    Console.WriteLine(currentPlayer + "Hvilken brik + kolonne vil du spille? Du kan vælge mellem 1-6"); // Player har ingen tostring, så her vil det være en idé at kalde currentPlayer.GetPlayerName()
+                    // spørg currentplayer hvilken brik de vil spille
+                    Console.WriteLine(currentPlayer.getPlayerName() + "Hvilken brik + kolonne vil du spille? Du kan vælge mellem 1-6");
 
-                    // var playerPick = Console.ReadLine(board.AddPiece(char formOfPiece, int kolonne));
-                    board.AddPiece(char formOfPiece, int kolonne); // Er første attribut ikke currentPlayer.GetFormOfPiece() ? Mens anden attribut er Console.Readline() ?
+                    board.AddPiece(currentPlayer.getFormOfPiece(), Convert.ToInt32(Console.ReadLine())-1); //Add'er brik til kolonne
+                }
+                else //hvis brættet er fuldt
+                {
+                    Console.WriteLine("Brættet er fuldt. Det blev uafgjort!");
+                    break; //afslutter loopet
                 }
 
                 // print pladen
-                Console.WriteLine(board.boardArray); // Jeg mener Lars lavede en toString så vi bare kan udskrive objectet...
+                Console.WriteLine(board.boardArray);
 
                 // Inden nextplayer kaldes tænker jeg GameOver klassen skal kaldes evt gennem et if statement
+
 
                 // nextplayer()
                 NextPlayer();
