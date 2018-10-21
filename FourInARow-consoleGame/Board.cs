c9c134547b842192800f305590d4db7048273eb7
Lars du Jour <lars66c1@edu.easj.dk>
2018-10-21 20:24:38 +0200

    BoardFull fix
    boardArray.length var en dum ide

--- FourInARow-consoleGame/Board.cs
+++ FourInARow-consoleGame/Board.cs
@@ -73,7 +73,7 @@ namespace FourInARow_consoleGame
         public bool BoardFull()
         {
             //tjekker alle kolonner. hvis alle returnerer -1, så er alle kolonner fulde, derfor er brættet fuldt
-            for (int i = 0; i < boardArray.Length; i++)
+            for (int i = 0; i < 6; i++)
             {
                 if (!KolonneFull(i))
                 {
